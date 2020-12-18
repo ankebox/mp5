3139b80418e9cc16a7fe4292bac7982e9cb4af20
David <davidaicardocossiopulido@gmail.com>
2020-12-18 10:09:23 +0100

    2

--- Track.java
+++ Track.java
@@ -14,6 +14,8 @@ public class Track
     // Where the track is stored.
     private String filename;
     
+    private int playCount;
+    
     /**
      * Constructor for objects of class Track.
      * @param artist The track's artist.
@@ -23,6 +25,7 @@ public class Track
     public Track(String artist, String title, String filename)
     {
         setDetails(artist, title, filename);
+        playCount = 0;
     }
     
     /**
@@ -34,6 +37,7 @@ public class Track
     public Track(String filename)
     {
         setDetails("unknown", "unknown", filename);
+        playCount = 0;
     }
     
     /**
@@ -69,7 +73,7 @@ public class Track
      */
     public String getDetails()
     {
-        return artist + ": " + title + "  (file: " + filename + ")";
+        return artist + ": " + title + "  (file: " + filename + ")" + "reproducido un total de " + playCount;
     }
     
     /**
@@ -83,6 +87,14 @@ public class Track
         this.artist = artist;
         this.title = title;
         this.filename = filename;
+    }
+    
+    public void resetPlayCount() {
+        playCount = 0;
+    }
+    
+    public void incrementPlayCount() {
+        playCount = playCount + 1;
     }
     
 }
